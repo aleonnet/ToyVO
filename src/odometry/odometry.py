08da237299e48479e1d1ec970b172eccaf7ca7a6
Matt Boler <meb0054@auburn.edu>
2019-03-28 22:35:47 -0500

    Expanded docstrings of methods in Camera

--- src/odometry/odometry.py
+++ src/odometry/odometry.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-class Camera:
+class Camera(object):
     """ A class representing a pinhole camera
 
     Attributes:
@@ -13,11 +13,36 @@ class Camera:
         self.K = K if K is not None else np.zeros((3,3))
         self.dist_coeffs = dist_coeffs if dist_coeffs is not None else np.array([0, 0, 0, 0, 0])
 
-    def undistort(self, image):
-        """Takes an image and returns the image undistorted"""
+    def undistortImage(self, image):
+        """Takes an image and returns the image undistorted
+
+        Args:
+            image (np.array): A distorted image (color/black and white?)
+
+        Returns:
+            undistorted_image (np.array): The image undistorted
+        """
+        return False
+
+    def undistortPoints(self, points2d):
+        """Takes pixel coordinates from a distorted image and undistortes them
+
+        Args:
+            points2d (np.array): Pixel coordinates of points in distorted image
+
+        Returns:
+            undistorted_points2d (np.array): Pixel coordinates of points in undistorted image
+        """
         return False
 
     def projectPoints(self, points3d):
-        """Returns the (u,v) pixel coordinates of 3d points in camera reference frame projected onto the image plane"""
+        """Projects 3d points onto the image plane.
+
+        Args:
+            points3d (np.array): An n-by-3 numpy array of 3d points in the camera frame
+
+        Returns:
+            points2d (np.array): An n-by-2 numpy array of pixel coordinates
+        """
         return False
     
